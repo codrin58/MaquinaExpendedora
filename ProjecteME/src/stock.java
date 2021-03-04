b63860d45871da0e8778db024ee7924bac1e082b
Joan <joancollm@inspladelestany.cat>
2021-03-04 17:33:33 +0100

    primera actualització stocks

---
+++ ProjecteME/src/stock.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class stock {
+	
+	/***
+	 * Saber si la contrassenya és correcte
+	 * @param Contrassenya introduïda
+	 * @return
+	 */
+	public static boolean accesMan(String pass) {
+		Scanner sc = new Scanner(System.in);
+		String contra = "pass123";
+		
+		System.out.println(); pass = sc.nextLine();
+		
+		if (pass == contra) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/***
+	 * Reemplenar stock a 12 productes
+	 * @param stock
+	 */
+	public static void rellenarStock(int[][] stock) {
+		for (int i=0; i < stock.length; i++) {
+            for (int j=0; j < stock.length; j++) {
+            	stock [i][j] = 12;
+            } 
+		}
+	}
+	/***
+	 * Revisem si hi ha stock amb les coordenades introduïdes per l'usuari
+	 * @param fil
+	 * @param col
+	 * @return
+	 */
+	/*public static boolean tenimstock(int fil, int col) {
+		int[][] stock = new int [fil][col];
+		
+		for (int i = 0; i < stock.length; i++) {
+			for (int j = 0; j < stock.length; j++) {
+
+			}
+		}
+		return true;
+	}*/
+	
+	public static void main(String[] args) {
+
+	}
+
+}
